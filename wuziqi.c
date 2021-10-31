1f4b1b18cc2dfb8c272fdc3d399813ff0fbeb570
EricJin2002 <769327841@qq.com>
2021-10-31 14:22:15 +0800

    add new cmd support

--- wuziqi.c
+++ wuziqi.c
@@ -70,6 +70,7 @@ void print_char(int i,int j){//https://zh.wikipedia.org/wiki/%E6%96%B9%E6%A1%86%
         }else{
             printf(j==1?"├":j==15?"┤":"┼");
         }
+        //if (j != 15) printf("─");//若使用新版控制台，取消注释这一行
     }else if(board[i][j]==-2){
         printf("×");
     }else if(board[i][j]%2){//black
@@ -369,5 +370,6 @@ int main(){
         //getchar();
         sleep(1);
     }
-    getchar();
+    printf("输入EOF退出");
+    while (getchar() != EOF);
 }
